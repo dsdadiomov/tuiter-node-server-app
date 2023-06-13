27b1556b94618d746fc280d26d8d0d73639ef17c
David <daviddadiomov@gmail.com>
2023-06-12 21:10:54 -0700

    idk

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-import express from 'express'
-import cors from 'cors'
-import HelloController from "./controllers/hello-controller.js"
-import UserController from "./users/users-controller.js"
+// creates/configures HTTP server listening for incoming requests
+
+import express from "express"; // creates an instance of the express library using ES6
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import AuthController from "./controllers/users/auth-controller.js";
+//  Cross Origin Resource Sharing -> establishes the rules by which resources can be shared across domains (origins)
+import cors from "cors";
 import session from "express-session";
-import AuthController from "./users/auth-controller.js";
 
+const app = express();
+app.use(express.json()); // for parsing application/json (helpful when we use ...req.body from json)
 
-
-const app = express()
 app.use(
-    session({
-      secret: "any string",
-      resave: false,
-      saveUninitialized: true,
-    })
-   );
-   
-app.use( cors({
+  // configure cors as middleware
+  // restrict cross origin resource sharing to the react application
+  cors({
     credentials: true,
     origin: "http://localhost:3000",
+  }),
+  // configure server session
+  session({
+    secret: "any string",
+    resave: false,
+    saveUninitialized: true,
+    store: new session.MemoryStore()
   })
- );
-app.use(express.json());
-const port = process.env.PORT || 4000;
+);
 
-
-TuitsController(app)
-HelloController(app)
-UserController(app)
-app.listen(process.env.PORT ||4000)
-
+HelloController(app);
+UserController(app);
+TuitsController(app);
 AuthController(app);
 
+app.listen(process.env.PORT || 4000);
 
 /* //const express = require('express');
 import express from 'express';
--- users/users-controller.js
+++ users/users-controller.js
@@ -1,55 +1,52 @@
-import people from './users.js'
-let users = people
+import people from "./users.js";
+let users = people;
+
 const UserController = (app) => {
-   app.get('/api/users', findUsers)
-   app.get('/api/users/:uid', findUserById);
-   app.post('/api/users', createUser);
-   app.delete('/api/users/:uid', deleteUser);
-   app.put('/api/users/:uid', updateUser); 
-
-}
-const createUser = (req, res) => {
-  const newUser = req.body;
-  newUser._id = (new Date()).getTime() + '';
-  users.push(newUser);
-  res.json(newUser);
-}
-const updateUser = (req, res) => {
-   const userId = req.params['uid'];
-   const updates = req.body;
-   users = users.map((usr) =>
-     usr._id === userId ?
-       {...usr, ...updates} :
-       usr
-   );
-   res.sendStatus(200);
-  }
-  
-
-const findUserById = (req, res) => {
-  const userId = req.params.uid;
-  const user = users
-    .find(u => u._id === userId);
-  res.json(user);
-}
-
-const deleteUser = (req, res) => {
-   const userId = req.params['uid'];
-   users = users.filter(usr => usr._id !== userId);
-   res.sendStatus(200);
- }
- 
+  app.get("/api/users", findUsers);
+  app.get("/api/users/:uid", findUsersById);
+  app.post("/api/users", createUser);
+  app.delete("/api/users/:uid", deleteUser);
+  app.put("/api/users/:uid", updateUser);
+};
 
 const findUsers = (req, res) => {
+    const type = req.query.type;
+    if (type) {
+        const usersOfType = users.filter(u => u.type === type);
+        res.json(usersOfType);
+        return;
+    } else {
+        res.json(users);
+    }
+};
 
-  const type = req.query.type
-  if(type) {
-    const usersOfType = users
-      .filter(u => u.type === type)
-    res.json(usersOfType)
-    return
-   }
+const findUsersById = (req, res) => {
+    const uid = req.params.uid;
+    const user = users.find((u) => u._id === uid);
+    res.json(user);
+};
 
-   res.json(users)
-}
-export default UserController
+const createUser = (req, res) => {
+    const newUser = req.body;
+    newUser._id = (new Date().getTime) + "";
+    users.push(newUser);
+    res.json(newUser);
+};
+
+const deleteUser = (req, res) => {
+    const userId = req.params['uid'];
+    let index = users.findIndex((usr) => usr._id === userId);
+    users.splice(index, 1);
+    res.sendStatus(200);
+};
+
+const updateUser = (req, res) => {
+    const userId = req.params.uid;
+    const updates = req.body;
+    users = users.map((user) =>
+        user._id === userId ? { ...user, ...updates } : user
+    );
+    res.sendStatus(200);
+};
+
+export default UserController;
