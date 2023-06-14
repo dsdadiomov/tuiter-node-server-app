82db7a16c2d460e298f8dbe452f8771e0a12a88b
David <daviddadiomov@gmail.com>
2023-06-14 05:20:04 -0700

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://idyllic-sunshine-7e8711.netlify.app",
   }),
   session({
     secret: "any string",
