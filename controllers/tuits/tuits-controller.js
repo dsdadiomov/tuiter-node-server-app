9ac27b0f38e5ee27044f927843430aed4a11535f
David <daviddadiomov@gmail.com>
2023-06-19 21:00:12 -0700

    qwe

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -7,33 +7,31 @@ const TuitsController = (app) => {
     app.delete('/api/tuits/:tid', deleteTuit);
 }
 
-const findTuits = async (req, res) => {
+const findTuits = async (req, res) => { 
     const tuits = await tuitsDao.findTuits()
-    res.json(tuits);
- }
- 
- const createTuit = async (req, res) => {
+    res.json(tuits) 
+}
+
+const createTuit = async (req, res) => {
     const newTuit = req.body;
     newTuit.likes = 0;
     newTuit.liked = false;
     const insertedTuit = await tuitsDao.createTuit(newTuit);
     res.json(insertedTuit);
-  }
-  
+}
 
-  const deleteTuit = async (req, res) => {
+const deleteTuit = async (req, res) => {
     const tuitdIdToDelete = req.params.tid;
     const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
     res.json(status);
-  }
-  
+}
 
-  const updateTuit = async (req, res) => {
-    const tuitdIdToUpdate = req.params.tid;
+const updateTuit = async (req, res) => {
+    const tuitdId = req.params.tid;
     const updates = req.body;
+    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
     res.json(status);
-  }
-  
+}
 
 
 export default TuitsController
